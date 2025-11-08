402957695da9b872e49fa1cbaba99b171b673408
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2025-11-08 12:04:57 -0500

    versio

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,10 @@
     <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">
 
+    <?php
+        $version = '1993.0.1';
+    ?>
+
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('logos/16x16.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('logos/32x32.png') }}">
     <link rel="icon" type="image/png" sizes="48x48" href="{{ asset('logos/48x48.png') }}">
@@ -35,15 +39,15 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-     <link rel="stylesheet" href="{{ asset('css/bootstrap-offcanvas.css') }}">
-    <link href="{{asset('css/wpp.css')}}" rel="stylesheet">
-    <link href="{{asset('lib/animate/animate.min.css')}}" rel="stylesheet">
-    <link href="{{asset('lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+     <link rel="stylesheet" href="{{ asset('css/bootstrap-offcanvas.css') }}?v=<?php echo $version ?>">
+    <link href="{{asset('css/wpp.css')}}?v=<?php echo $version ?>" rel="stylesheet">
+    <link href="{{asset('lib/animate/animate.min.css')}}?v=<?php echo $version ?>" rel="stylesheet">
+    <link href="{{asset('lib/owlcarousel/assets/owl.carousel.min.css')}}?v=<?php echo $version ?>" rel="stylesheet">
 
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="{{asset('css/style.css')}}" rel="stylesheet">
-    <link href="{{asset('css/phone.css')}}" rel="stylesheet">
+    <link href="{{asset('css/style.css')}}?v=<?php echo $version ?>" rel="stylesheet">
+    <link href="{{asset('css/phone.css')}}?v=<?php echo $version ?>" rel="stylesheet">
 
     <!-- Scripts -->
     <!-- vite(['resources/sass/app.scss', 'resources/js/app.js']) -->
@@ -58,19 +62,19 @@
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-    <script src="{{asset('lib/easing/easing.min.js')}}"></script>
-    <script src="{{asset('lib/owlcarousel/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('lib/easing/easing.min.js')}}?v=<?php echo $version ?>"></script>
+    <script src="{{asset('lib/owlcarousel/owl.carousel.min.js')}}?v=<?php echo $version ?>"></script>
 
     <!-- Contact Javascript File -->
-    <script src="{{asset('mail/jqBootstrapValidation.min.js')}}"></script>
-    <script src="{{asset('mail/contact.js')}}"></script>
+    <script src="{{asset('mail/jqBootstrapValidation.min.js')}}?v=<?php echo $version ?>"></script>
+    <script src="{{asset('mail/contact.js')}}?v=<?php echo $version ?>"></script>
 
     <!-- Template Javascript -->
-    <script src="{{asset('js/main.js')}}"></script>
-    <script src="{{asset('js/addcart.js')}}"></script>
-    <script src="{{asset('js/phone.js')}}"></script>
-    <script src="{{asset('js/ubigeo.js')}}"></script>
-    <script src="{{asset('js/buscar.js')}}"></script>
+    <script src="{{asset('js/main.js')}}?v=<?php echo $version ?>"></script>
+    <script src="{{asset('js/addcart.js')}}?v=<?php echo $version ?>"></script>
+    <script src="{{asset('js/phone.js')}}?v=<?php echo $version ?>"></script>
+    <script src="{{asset('js/ubigeo.js')}}?v=<?php echo $version ?>"></script>
+    <script src="{{asset('js/buscar.js')}}?v=<?php echo $version ?>"></script>
     @stack('scripts')
 </body>
 </html>
